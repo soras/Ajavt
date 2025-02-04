34013a2c9e4605dcc4791df7f333a79a6c33de28
Siim Orasmaa <siim.orasmaa@ut.ee>
2025-02-04 15:25:20 +0200

    Updated license: GPL-2.0 -> GPL-2.0 or Apache-2.0

--- src/ee/ut/soras/ajavtV2/AjaTuvastaja.java
+++ src/ee/ut/soras/ajavtV2/AjaTuvastaja.java
@@ -1,17 +1,20 @@
 //  Ajavt: Temporal Expression Tagger for Estonian
-//  Copyright (C) 2009-2016  University of Tartu
+//  Copyright (C) 2009-2015  University of Tartu
 //  Author:   Siim Orasmaa
 //  Contact:  siim . orasmaa {at} ut . ee
 //  
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
+//  This program is released under dual license: either GNU General 
+//  Public License v2.0 or Apache 2.0 License. 
+//
+//  Full copy of GNU General Public License v2.0 can be found at 
+//  http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html .
+//
+//  Full copy of Apache 2.0 License can be found at 
+//  http://www.apache.org/licenses/LICENSE-2.0 .
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
 package ee.ut.soras.ajavtV2;
 
--- src/ee/ut/soras/ajavtV2/Main.java
+++ src/ee/ut/soras/ajavtV2/Main.java
@@ -3,15 +3,18 @@
 //  Author:   Siim Orasmaa
 //  Contact:  siim . orasmaa {at} ut . ee
 //  
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
+//  This program is released under dual license: either GNU General 
+//  Public License v2.0 or Apache 2.0 License. 
+//
+//  Full copy of GNU General Public License v2.0 can be found at 
+//  http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html .
+//
+//  Full copy of Apache 2.0 License can be found at 
+//  http://www.apache.org/licenses/LICENSE-2.0 .
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
 package ee.ut.soras.ajavtV2;
 
@@ -306,6 +309,12 @@ public class Main {
 		// --------------------------------------------------------------------
 		//   *) V2ljund
 		// --------------------------------------------------------------------
+        //if (fullDebug || partialDebug){
+        //    for (int i = 0; i < tulemAjavtSonad.size(); i++) {
+        //        AjavtSona sona = tulemAjavtSonad.get(i);
+        //        System.out.println(' '+sona.getAlgSonaErisymbolidNormaliseeritud());
+        //    }
+        //}
 		try {
 			if (outputType.equalsIgnoreCase("stdout")){
 				PrintStream ps = new PrintStream(System.out, false, "UTF-8");
